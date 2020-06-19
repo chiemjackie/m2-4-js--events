8fa965e693cde51ffbc48e95c4ecd0335d35a6a9
Jackie Chiem <32667298+chiemjackie@users.noreply.github.com>
2020-06-19 11:43:32 -0400

    Completed up to 2.3, 2.4 is just copied solution

--- workshop/exercise-2.3/app.js
+++ workshop/exercise-2.3/app.js
@@ -1,4 +1,4 @@
-const main = document.getElementById('main');
+const main = document.getElementById('main')
 
 function toggleColour (event) {
   const buttonId = event.target.id;
@@ -7,9 +7,11 @@ function toggleColour (event) {
 
 for (let i = 1; i <= 20; i++) {
   const button = document.createElement('button');
+  main.appendChild(button);
+  button.id= `btn-${i}`;
   button.innerText = i;
-  button.id = `btn-${i}`;
-  mainDiv.appendChild(button);
+  button.style.top = `${Math.random() * 95}vh`;
+  button.style.left = `${Math.random() * 95}vw`;
 
   button.addEventListener('click', toggleColour);
 }
